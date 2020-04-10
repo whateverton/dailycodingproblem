083b44e626e1a3f892e17f7e51b417dcd0d2cc4b
whateverton <everton.vilela@live.com>
2020-04-10 20:21:48 -0300

    Brought print back and corrected some mistakes.

--- src/16-EASY/16-EASY.cpp
+++ src/16-EASY/16-EASY.cpp
@@ -25,7 +25,7 @@ public:
         log = (int*)malloc (N * sizeof(int));
 
         log_size = N;
-        head = 0;
+        head = -1;
         tail = 0;
     }
 
@@ -35,23 +35,49 @@ public:
             free(log);
     }
 
+    void print()
+    {
+        if (log_size == 0)
+            cout << "No Order ID to show!\n";
+        else
+        {
+            cout << "Complete Order ID Log (in order of insertion):\n";
+
+            for (int i = 0, elem = tail; i < log_size; ++i, ++elem)
+            {
+                if (elem >= log_size)
+                    elem = 0;
+
+                cout << log[elem];
+
+                if (elem != head)
+                    cout << ", ";
+                else
+                {
+                    cout << endl;
+                    break;
+                }
+            }
+        }
+    }
+
     bool record(int order_id)
     {
-        if (++head >= log_size)
-        {
-            head = 0;
-
-            if (log_size > 1)
-                ++tail;
-        }
-        
         if (tail != 0)
         {
             if (++tail >= log_size)
                 tail = 0;
         }
 
-        log[tail] = order_id;
+        if ((++head >= log_size))
+        {
+            head = 0;
+
+            if (log_size > 1)
+                ++tail;
+        }
+
+        log[head] = order_id;
 
         return true;
     }
@@ -61,21 +87,21 @@ public:
          if ((log_size == 0) || (last == NULL))
             return false;
 
-        *last = log[tail];
+        *last = log[head];
         
         return true;
     }
 
 private:
-    unsigned int log_size;
-    unsigned int head;
-    unsigned int tail;
+    int log_size;
+    int head;
+    int tail;
     int* log;
 };
 
 int main()
 {
-    OrderIdLog my_log(1);
+    OrderIdLog my_log(5);
     int last;
 
     my_log.record(1);
@@ -84,6 +110,8 @@ int main()
         my_log.record(i);
 
     my_log.record(47);
+
+    my_log.print();
 
     if (my_log.get_last(&last))
         cout << "Last logged element: " << last << endl;
