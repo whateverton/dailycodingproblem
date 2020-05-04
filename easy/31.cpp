5f2928c5a1cf94428238bd6339731ac3880c4fb7
whateverton <evertonandradevilela@gmail.com>
2020-05-04 03:19:29 -0300

    Problem 31 - EASY - Test Cases

--- easy/31.cpp
+++ easy/31.cpp
@@ -7,11 +7,13 @@ The edit distance between two strings refers to the minimum number of character 
 Given two strings, compute the edit distance between them.
 
 Date: 04/05/2020
-Dev Time: 2h47 - 3h05 = Nonsense logic, works only with the example
+Dev Time: 2h47 - 3h05 = 18min - Nonsense logic, works only with the example
+		  3h08 - 3h18 = 10min - Creating a testing system that makes my code fail
 */
 #include <string>
 
 #define min_value(x,y) ((x < y) ? x : y)
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 int getEditDistance(std::string s1, std::string s2)
 {
@@ -36,7 +38,19 @@ int getEditDistance(std::string s1, std::string s2)
 #ifdef _31_EASY_
 void CurrentSolution()
 {
-	std::cout << "The edit distance is: " << getEditDistance("kitten", "sitting") << std::endl;
+	struct test_struct {
+		std::string s1;
+		std::string s2;
+		int distance;
+	} test_strings [] = { {"kitten", "sitting", 3},
+						  {"cat", "gap", 2},
+						  {"keep", "sweep", 2} };
+
+	for (int i = 0; i < ARRAY_SIZE(test_strings); ++i)
+	{
+		std::cout << "The edit distance between '" << test_strings[i].s1 << "' and '" << test_strings[i].s2 << "' is: " << getEditDistance(test_strings[i].s1, test_strings[i].s2) << ". And it should be: " << test_strings[i].distance << std::endl;
+		std::cout << "The edit distance between '" << test_strings[i].s2 << "' and '" << test_strings[i].s1 << "' is: " << getEditDistance(test_strings[i].s2, test_strings[i].s1) << ". And it should be: " << test_strings[i].distance << std::endl;
+	}
 
 }
 #endif
